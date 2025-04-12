4cf3bfe1b1d9a1c103c52207c7658e8d1920977d
Martin-DMC <the.chiperfil@gmail.com>
2025-04-12 15:49:52 +0000

    optimizando bubblesort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,31 +1,35 @@
 #include "sort.h"
 #include <stdio.h>
 #include <stddef.h>
-
 /**
+ * bubble_sort - implementa el algoritmo bubble sort para ordenar
+ * @array: puntero al array a ordenar
+ * @size: tamaño del array a ordenar
  */
 void bubble_sort(int *array, size_t size)
 {
-	int *tmp;
-	size_t it = 0, change, loop = 0;
+	int tmp;
+	size_t it = 0, j, change;
 
-	while (it < (size - loop))
+	if (array == NULL || size < 2)
+		return;
+	while (it < (size - 1))
 	{
 		change = 0;
-		while (it <= (size - loop))
+		j = 0;
+		while (j < (size - it - 1))
 		{
-			if (array[it] > array[it + 1])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[it];
-				array[it] = array[it + 1];
-				array[it + 1] = tmp;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				change = 1;
+				print_array(array, size);
 			}
 			j++;
 		}
-		if (change != 0)
-			print_array(array, size);
-		loop++;
-		it++;
+		if (change == 0)
+			break;
 	}
 }
